8aab336190d8534490fc0c7d5e308ea24fc7cf5c
Enrique Mireles <46823346+emirelesg@users.noreply.github.com>
2023-10-22 01:00:26 +0200

    edit: clean code

--- blink/src/sim/top_tb.sv
+++ blink/src/sim/top_tb.sv
@@ -3,16 +3,16 @@
 module top_tp;
     logic clk = 0;
     logic reset_n = 1;
-    logic out = 0;
+    logic out;
 
-    // N = 100 Mhz / 100 kHz - 1
-    // N = 1e8 / 1e5 - 1
-    // N = 1000 - 1
-    top #(.N(999)) uut(
+    // N = 100 Mhz / 200 kHz - 1
+    // N = 1e8 / 2e5 - 1
+    // N = 500 - 1
+    top #(.N(499)) uut(
         .clk(clk),
         .reset_n(reset_n),
         .led0_b(out)
     );
     
-    always #5 clk = ~clk; // Simulate a 100 Mhz clk.
+    always #5 clk = ~clk; // Simulate a 100 Mhz signal.
 endmodule
