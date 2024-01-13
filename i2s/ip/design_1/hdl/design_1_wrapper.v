40fc50e9a0889dc502ba6c11d74c6072057bfe65
Enrique Mireles <46823346+emirelesg@users.noreply.github.com>
2024-01-13 20:55:46 +0100

    edit: customize and expose `clk_i2s`

--- i2s/ip/design_1/hdl/design_1_wrapper.v
+++ i2s/ip/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Sat Jan 13 20:22:44 2024
+//Date        : Sat Jan 13 20:53:12 2024
 //Host        : PC-ENRIQUE running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
@@ -11,14 +11,18 @@
 
 module design_1_wrapper
    (clk,
+    clk_i2s,
     reset_n);
   input clk;
+  output clk_i2s;
   input reset_n;
 
   wire clk;
+  wire clk_i2s;
   wire reset_n;
 
   design_1 design_1_i
        (.clk(clk),
+        .clk_i2s(clk_i2s),
         .reset_n(reset_n));
 endmodule
--- i2s/src/rtl/top.sv
+++ i2s/src/rtl/top.sv
@@ -3,5 +3,15 @@ module top
         input logic clk,
         input logic reset_n
     );
-
+    
+    /* ~~ Create design_1_wrapper unit ~~ */
+    
+    logic clk_i2s;
+    
+    design_1_wrapper design_1_wrapper_unit (
+        .clk(clk),
+        .reset_n(reset_n),
+        // Outputs
+        .clk_i2s(clk_i2s)
+    );
 endmodule
