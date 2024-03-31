6866bfcc3e576c2d4285ea5a25633aee80d75a4b
Enrique Mireles <46823346+emirelesg@users.noreply.github.com>
2024-03-31 01:08:33 +0100

    feat: add `en`  and `data_valid` signals

--- ddfs/src/rtl/ddfs.sv
+++ ddfs/src/rtl/ddfs.sv
@@ -1,29 +1,33 @@
 module ddfs
     #(
-        parameter   PHASE_WIDTH = 30    // 30-bit phase accumulator
+        parameter   PHASE_WIDTH = 30,   // 30-bit phase accumulator
+                    ADDR_WIDTH = 11     // 2048 values
     )
     (
         input logic clk,
         input logic reset_n,
+        input logic en,                     // Enable signal to generate a new sample.
         input logic [PHASE_WIDTH-1:0] fccw, // Carrier frequency control word
         input logic [PHASE_WIDTH-1:0] focw, // Frequency offset control word
         input logic [PHASE_WIDTH-1:0] pha,  // Phase offset
         input logic [15:0] env, // Q2.14    // Envelope
         output logic [15:0] pcm_out,
-        output logic pulse_out
+        output logic pulse_out,
+        output logic data_valid
     );
-    
-    logic [7:0] p2a_r_addr;
+
+    logic [ADDR_WIDTH-1:0] p2a_r_addr;
     logic [15:0] amp;
-    
+
     logic [PHASE_WIDTH-1:0] pcw;
     logic [PHASE_WIDTH-1:0] fcw;
     logic [PHASE_WIDTH-1:0] p_reg, p_next;
-    
+
     logic signed [31:0] modulated; // Q18.14
-    logic [15:0] pcm_reg; // Q16.0
-        
-    sin_rom sin_rom_unit(
+    logic [15:0] pcm_reg, pcm_next; // Q16.0
+    logic valid_reg, valid_next;
+
+    sin_rom #(.ADDR_WIDTH(ADDR_WIDTH)) sin_rom_unit(
         .clk(clk),
         .r_addr(p2a_r_addr),
         .r_data(amp)
@@ -33,30 +37,46 @@ module ddfs
         if (~reset_n) begin
             p_reg <= 0;
             pcm_reg <= 0;
+            valid_reg <= 1'b0;
         end
         else begin
             p_reg <= p_next;
-            // To trim the multiplication of two 16-bit signed integers,
-            // use the 16 MSBs of the modulated value.
-            pcm_reg <= modulated[29:14];
+            pcm_reg <= pcm_next;
+            valid_reg <= valid_next;
         end
-    
+
     // Frequence modulation
     assign fcw = fccw + focw;
-    
-    // Phase accumulation
-    assign p_next = p_reg + fcw;
-    
+
     // Phase modulation
     assign pcw = p_reg + pha;
-    
+
     // Phase to amplitude mapping address
-    // Use the 8 MSBs of the PCW value as the address to look-up the SINE value.
-    assign p2a_r_addr = pcw[PHASE_WIDTH-1:PHASE_WIDTH-8];
-    
+    // Use the ADDR_WIDTH MSBs of the PCW value as the address to look-up the SINE value.
+    assign p2a_r_addr = pcw[PHASE_WIDTH-1:PHASE_WIDTH-ADDR_WIDTH];
+
     // Amplitude modulation
     assign modulated = $signed(env) * $signed(amp);
-    
+
+    always_comb begin
+        p_next = p_reg;
+        pcm_next = pcm_reg;
+        valid_next = 1'b0;
+
+        if (en) begin
+            // Phase accumulation
+            p_next = p_reg + fcw;
+
+            // To trim the multiplication of two 16-bit signed integers,
+            // use the 16 MSBs of the modulated value.
+            pcm_next = modulated[29:14];
+
+            // Generate a tick when the data changes. Used to interface with a fifo as a wr_en signal.
+            valid_next = 1'b1;
+        end
+    end
+
     assign pcm_out = pcm_reg;
     assign pulse_out = p_reg[PHASE_WIDTH-1];
+    assign data_valid = valid_reg;
 endmodule
--- ddfs/src/sim/top_tb.sv
+++ ddfs/src/sim/top_tb.sv
@@ -3,36 +3,62 @@
 module top_tb;
     localparam T=10; // 100 Mhz, 10 ns
 
+    logic [1:0] clk_divider; // 2 bits used for a /4 divider.
     logic clk;
     logic reset_n;
+    logic en;
+    logic [29:0] fccw, focw;
+    logic [29:0] pha;
+    logic [15:0] env;
+    logic [15:0] pcm_out;
+    logic data_valid;
 
-    top uut(
+    ddfs uut(
         .clk(clk),
-        .reset_n(reset_n)
+        .reset_n(reset_n),
+        .en(en),
+        .fccw(fccw),
+        .focw(focw),
+        .pha(pha),
+        .env(env),
+        // Outputs
+        .pcm_out(pcm_out),
+        .data_valid(data_valid)
     );
 
-    // Simulate a 100 Mhz clock signal.
-    always begin
-        clk = 1'b0;
-        #(T/2);
-        clk = 1'b1;
-        #(T/2);
-    end
+    // Simulate a 100 MHz clock signal.
+    initial clk = 0;
+    always clk = #(T/2) ~clk;
+
+    // Simulate a tick at 25 MHz intervals.
+    always_ff @(posedge clk)
+        if (~reset_n)
+            clk_divider <= 0;
+        else
+            clk_divider <= clk_divider + 1;
+    assign en = &clk_divider;
 
     // Reset at the start of the simulation.
     initial begin
         reset_n = 1'b0;
-        #(T/2);
+        @(negedge clk);
         reset_n = 1'b1;
-        #(T);
+    end
+
+    // Initial values for signals.
+    initial begin
+        // (2 ^ PHASE_WIDTH * freq / 25_000_000)
+        fccw = 42949; // 1000 Hz
+        focw = 0;
+        pha = 0;
+        env = 16'h4000; // 1.0
     end
 
     initial begin
         @(posedge reset_n); // Wait for the reset.
         @(negedge clk);
-        
-        #5ms;
 
-        $stop;
+        #2ms; // 1 full wave cycle at 1 kHz
+        $finish;
     end
 endmodule
