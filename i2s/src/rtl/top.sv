b8fc3abaae1e4c15962395db617b8d45a55e6542
Enrique Mireles <46823346+emirelesg@users.noreply.github.com>
2024-05-12 17:25:25 +0200

    feat: generate sample frequencies

--- i2s/src/rtl/top.sv
+++ i2s/src/rtl/top.sv
@@ -25,29 +25,18 @@ module top
     );
 
     logic audio_valid_in, audio_valid_out;
+    logic audio_96_tick;
     logic [`DATA_BIT-1:0]
         audio_l_in, audio_r_in,
         audio_l_out, audio_r_out;
 
     logic ddfs_data_valid;
 	logic [15:0] pcm_out;
-    logic [29:0] fccw, focw; // (2 ^ PHASE_WIDTH * freq / 96_000)
+    logic [29:0] fccw, focw;
     logic [29:0] pha;
     logic [15:0] env;
     logic [2:0] wave_type;
-
-    always_comb begin
-        if (i_sw == 4'b0001)
-            fccw = 447_392; // 40 Hz
-        else
-            if (i_sw == 4'b0010)
-                fccw = 894_784; // 80 Hz
-             else
-                if (i_sw == 4'b0100)
-                    fccw = 1_789_569; // 160 Hz
-                else
-                    fccw = 3_579_139; // 320 Hz
-    end
+    logic [29:0] ffcw_array [0:15];
 
     initial begin
         focw = 0;
@@ -56,11 +45,23 @@ module top
         wave_type = 3'b001; // Sine
     end
 
+    // Use the sw to lookup a frequency multiple of 40Hz.
+    assign fccw = ffcw_array[i_sw];
+
+    genvar i;
+    generate
+        for (i = 0; i < 16; i++) begin
+            initial begin
+                ffcw_array[i] = $rtoi(2**30 * (i * 40.0 / 96_000.0)); // (2 ^ PHASE_WIDTH * freq / 96_000)
+            end
+        end
+    endgenerate
+
     // Example to generate a sine wave:
     ddfs ddfs_unit(
         .i_clk(i_clk),
         .i_reset_n(i_reset_n),
-        .i_en(audio_valid_out),
+        .i_en(audio_96_tick),
         .i_fccw(fccw),
         .i_focw(focw),
         .i_pha(pha),
@@ -78,7 +79,7 @@ module top
 		.i_clk_12_288(clk_12_288),
 		.i_reset_n(i_reset_n),
 		.i_audio_l(audio_l_in),
-        .i_audio_r(audio_r_in),
+        .i_audio_r(audio_l_in),
         .i_audio_valid(audio_valid_in),
 		// Outputs
 		.o_audio_l(audio_l_out),
@@ -91,9 +92,10 @@ module top
         .i_rx_sd(i_rx_sd)
 	);
 
-    assign audio_l_in = audio_l_out;
-    assign audio_r_in = audio_r_out;
-    assign audio_valid_in = audio_valid_out;
+    assign audio_l_in = pcm_out;
+    assign audio_r_in = pcm_out;
+    assign audio_valid_in = ddfs_data_valid;
+    assign audio_96_tick = audio_valid_out;
 
 	assign o_tx_mclk = mclk;
     assign o_rx_mclk = mclk;
